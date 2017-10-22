f080c58bf0b1b188b55285b5655d2ea0d78a5bdf
abhayvjoshi <joshi.abhayv05@gmail.com>
2017-10-22 11:57:02 -0700

    Adding new matrix
    Graduate and speialization

--- js/mainController.js
+++ js/mainController.js
@@ -26,14 +26,18 @@ function ($http,$scope,$route,$rootScope, $moment){
     var countyMap = [];
     $scope.schoolList = [];
     $scope.metricMap = {};
-    $scope.countyTitle = ''
+    $scope.countyTitle = '';
+    $scope.subjectSpecial = '';
+    $scope.gradLevel = '';
+    $scope.highly_Qualify_Total_Count = 0;
+    $scope.highly_Qualify_Count = 0;
     $scope.toggleYear = new Date().getFullYear().toString();
     $scope.tickMax = 1;
 
     $scope.failedChart = {
         type: 'horizontalBar',
         labels: ['Teachers','Schools'],
-        colors: [ '#c4c4c4']
+        colors: [ '#f9214a']
     };
 
     function generateDataForChart(map,year){
@@ -41,7 +45,7 @@ function ($http,$scope,$route,$rootScope, $moment){
     }
 
     $scope.hoverOver = function(element){
-        $scope['element' + element.target.id] = {'fill':'blue'};
+        $scope['element' + element.target.id] = {'fill':'#f9214a'};
     }
 
     function getCountyFilter(){
@@ -68,9 +72,50 @@ function ($http,$scope,$route,$rootScope, $moment){
         var thisYear = new Date().getFullYear().toString();
         delete previousYears[thisYear];
         $scope.newTeachNSchool = getNewDatas($scope.metricMap[thisYear], previousYears);
-        $scope.dataSet = [(choseTime['teachers']).unique().length, (choseTime['schools']).unique().length],
-        
-
+        console.log($scope.newTeachNSchool);
+        $scope.dataSet = [(choseTime['teachers']).unique().length, (choseTime['schools']).unique().length];
+        var highlyQualifyTotalCount = $scope.metricMap[thisYear]['highly_qualify'].length;
+        var highlyQualifyCount = 0;
+        for(var i=0;i<highlyQualifyTotalCount;i++)
+        {
+        	if ($scope.metricMap[thisYear]['highly_qualify'][i]=='Y') {
+        		highlyQualifyCount++;
+        	}
+        }
+        $scope.highly_Qualify_Total_Count = highlyQualifyTotalCount;
+    	$scope.highly_Qualify_Count = highlyQualifyCount;
+        var modeGradMap = {};
+        var modeSplMap = {};
+	    var maxGradEl = $scope.metricMap[thisYear]['graduate_level'][0], maxGradCount = 1, maxSplCount = 1;
+	    var maxSplEl = $scope.metricMap[thisYear]['specialization'][0];
+	    for(var i = 0; i < $scope.metricMap[thisYear]['graduate_level'].length; i++)
+	    {
+	        var el = $scope.metricMap[thisYear]['graduate_level'][i];
+	        if(modeGradMap[el] == null)
+	            modeGradMap[el] = 1;
+	        else
+	            modeGradMap[el]++;  
+	        if(modeGradMap[el] > maxGradCount)
+	        {
+	            maxGradEl = el;
+	            maxGradCount = modeGradMap[el];
+	        }
+	    }
+	    for(var i = 0; i < $scope.metricMap[thisYear]['specialization'].length; i++)
+	    {
+	        var el = $scope.metricMap[thisYear]['specialization'][i];
+	        if(modeSplMap[el] == null)
+	            modeSplMap[el] = 1;
+	        else
+	            modeSplMap[el]++;  
+	        if(modeSplMap[el] > maxGradCount)
+	        {
+	            modeSplEl = el;
+	            modeSplCount = modeSplMap[el];
+	        }
+	    }
+	    $scope.subjectSpecial = maxSplEl;
+        $scope.gradLevel = maxGradEl;
         $scope.countyTitle = county;
         $scope.tickMax = $scope.dataSet[0] + ($scope.dataSet[0]/10);
         $scope.showDropdown = true;
@@ -101,19 +146,26 @@ function ($http,$scope,$route,$rootScope, $moment){
     function computeYearlyMetric(map,county){
         var metricMap = {'county':county };
         var keys = {};
+        console.log(map);
         for(var i = 0; i < map.length; i ++){
             var date = new Date(map[i].created_date);
             var school = map[i].school;
             var name = map[i].first_name +' '+ map[i].middle_name +' '+ map[i].last_name;
+            var specialization = map[i].subject_spl;
+            var graduate_status = map[i].graduate_level;
             if(!metricMap[date.getFullYear()]){
                 metricMap[date.getFullYear()] = {};
                 metricMap[date.getFullYear()]['teachers'] = [name];
                 metricMap[date.getFullYear()]['schools'] = [school];
                 metricMap[date.getFullYear()]['highly_qualify'] = [map[i].highly_qualify];
+                metricMap[date.getFullYear()]['specialization'] = [specialization];
+                metricMap[date.getFullYear()]['graduate_level'] = [graduate_status];
             }else{
                 metricMap[date.getFullYear()]['teachers'].push(name);
                 metricMap[date.getFullYear()]['schools'].push(school);
                 metricMap[date.getFullYear()]['highly_qualify'].push(map[i].highly_qualify);
+                metricMap[date.getFullYear()]['specialization'].push(specialization);
+                metricMap[date.getFullYear()]['graduate_level'].push(graduate_status);
             }
         }
         $scope.metricMap = metricMap;
@@ -172,14 +224,17 @@ function ($http,$scope,$route,$rootScope, $moment){
                     schoolNames.push(schools[i].name);
                 }
             }
+            var subject_special = ['Social Studies','Visual Arts','English','Finance','Economics','Photography','Mathematics','Marketing','History'];
+            var graduate_level = ['Elementary school','Middle school', 'High school', 'College'];
             for(var i = 0; i < $scope.jsonData.length; i ++){
                 $scope.jsonData[i]['created_date'] = randYears[getRandomBtwn(0,2)];
                 $scope.jsonData[i]['school'] = schoolNames[getRandomBtwn(0,schoolNames.length-1)];
                 $scope.jsonData[i]['highly_qualify'] = qualify[getRandomBtwn(0,qualify.length-1)];
                 $scope.jsonData[i]['reduce_lunch'] = getRandomBtwn(0,100) + '%';
+             	$scope.jsonData[i]['subject_spl'] = subject_special[getRandomBtwn(0,subject_special.length-1)];
+             	$scope.jsonData[i]['graduate_level'] = graduate_level[getRandomBtwn(0,graduate_level.length-1)];   
             }
-
-            console.log($scope.jsonData);
+            //console.log($scope.jsonData);
         }); 
     }; 
 
